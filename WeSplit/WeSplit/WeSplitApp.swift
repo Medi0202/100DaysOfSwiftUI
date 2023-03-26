18b102e11c479517db99a151823f990187c8691f
Jae Ho Yoon <mr.jaeho@gmail.com>
2023-03-26 17:51:58 +0900

    init

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jae Ho Yoon on 2023/03/26.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
